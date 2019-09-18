5ff221400c19583ef03daac09e5f3d8c925ff98b
ramnrem <ramnrem08@gmail.com>
2019-09-18 22:44:20 +0300

    page

---
+++ vendor/app.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+
+	$.each($('.zig-zag-slider'), function(i, el){
+		$(el).slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			nextArrow: $(el).next(),
+			prevArrow:  $(el).prev(),
+			responsive: [
+				{
+					breakpoint: 991,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+			]
+		});
+	});
+	
+	$.each($('.announcements-list'), function(i, el){
+		$(el).slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			prevArrow: $(el).prev().find('.announcements-list__prev'),
+			nextArrow:  $(el).prev().find('.announcements-list__next'),
+			responsive: [
+				{
+					breakpoint: 991,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+			]
+		});
+	});
+	
+
+	$('.nav-col').on('click', function(){
+		$(this).find('.nav-col__list').length 
+			? $(this).find('.nav-col__list').slideToggle()
+			: $(this).next().slideToggle();
+
+		$(this).toggleClass('active');
+	});
+
+	$('#hamb').on('click', function(){
+		$('#mobile-navigation').addClass('active');
+	});
+
+	$('.close').on('click', function(){
+		$('#mobile-navigation').removeClass('active');
+	})
+
+	$(window).on('resize', function() {
+		if(window.innerWidth > 991) {
+			$('.nav-col__list').slideDown();
+			$('.nav-col').removeClass('active');
+			$('.footer__soc').slideDown();
+			$('.single-line-form').slideDown();
+		}
+	});
+})
